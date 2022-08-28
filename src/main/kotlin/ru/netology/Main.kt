77870ec5af235e1ba277c279938a86acf9e2ee79
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-29 00:00:08 +0300

    Первоначальная версия

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,2 +1,42 @@
 package ru.netology
 
+//enum class NameOfTime {
+//    Seconds, Minutes, Hours, Days
+//}
+const val MINUTE = 60
+const val HOUR = 3_600
+const val DAY = 86_400
+
+
+fun main() {
+    val result = agoToText(86500)
+    println("был(а) в сети $result")
+}
+
+
+fun agoToText(
+    secondsCount: Int
+): String = when {
+    (secondsCount < MINUTE) -> "только что"
+    ((MINUTE + 1) < secondsCount && secondsCount < HOUR) -> "${amountOfTime(secondsCount)} минут назад"
+    ((HOUR + 1) < secondsCount && secondsCount < DAY) -> "${amountOfTime(secondsCount)} часов назад"
+    ((DAY + 1) < secondsCount && secondsCount < DAY * 2) -> "сегодня"
+    ((DAY * 2 + 1) < secondsCount && secondsCount < DAY * 3) -> "вчера"
+    else -> "давно"
+}
+
+fun amountOfTime(
+    secondsCount: Int
+): Int {
+
+    return when {
+        ((MINUTE + 1) < secondsCount && secondsCount < HOUR) -> secondsCount / MINUTE
+        ((HOUR + 1) < secondsCount && secondsCount < DAY) -> secondsCount / HOUR
+
+        else -> secondsCount
+    }
+}
+
+
+
+
