4ed7a69e5634812b751d865d338094d21ad63b7c
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-29 00:57:39 +0300

    Добавлена информация о последнем посещении страницы

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,15 +1,14 @@
 package ru.netology
 
-//enum class NameOfTime {
-//    Seconds, Minutes, Hours, Days
-//}
 const val MINUTE = 60
 const val HOUR = 3_600
 const val DAY = 86_400
 
 
 fun main() {
-    val result = agoToText(86500)
+
+
+    val result = agoToText(4650)
     println("был(а) в сети $result")
 }
 
@@ -18,8 +17,8 @@ fun agoToText(
     secondsCount: Int
 ): String = when {
     (secondsCount < MINUTE) -> "только что"
-    ((MINUTE + 1) < secondsCount && secondsCount < HOUR) -> "${amountOfTime(secondsCount)} минут назад"
-    ((HOUR + 1) < secondsCount && secondsCount < DAY) -> "${amountOfTime(secondsCount)} часов назад"
+    ((MINUTE + 1) < secondsCount && secondsCount < HOUR) -> "${amountOfTime(secondsCount)} ${endingOfWord(secondsCount)} назад"
+    ((HOUR + 1) < secondsCount && secondsCount < DAY) -> "${amountOfTime(secondsCount)} ${endingOfWord(secondsCount)} назад"
     ((DAY + 1) < secondsCount && secondsCount < DAY * 2) -> "сегодня"
     ((DAY * 2 + 1) < secondsCount && secondsCount < DAY * 3) -> "вчера"
     else -> "давно"
@@ -37,6 +36,29 @@ fun amountOfTime(
     }
 }
 
+fun endingOfWord(
+    secondsCount: Int
+): String {
+    return when {
+        ((MINUTE + 1) < secondsCount && secondsCount < HOUR) ->
+            when {
+                (amountOfTime(secondsCount) % 10 === 1 && amountOfTime(secondsCount) % 100 !== 11) -> "минуту"
+                (amountOfTime(secondsCount) % 10 === 2 || amountOfTime(secondsCount) % 10 === 3 || amountOfTime(
+                    secondsCount
+                ) % 10 === 4) -> "минуты"
+                else -> "минут"
+            }
+        ((HOUR + 1) < secondsCount && secondsCount < DAY) ->
+            when {
+                (amountOfTime(secondsCount) % 10 === 1 && amountOfTime(secondsCount) % 100 !== 11) -> "час"
+                (amountOfTime(secondsCount) % 10 === 2 || amountOfTime(secondsCount) % 10 === 3 || amountOfTime(
+                    secondsCount
+                ) % 10 === 4) -> "часа"
+                else -> "часов"
+            }
+        else -> ""
+    }
+}
 
 
 
